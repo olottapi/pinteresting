38696e7f695ef22dc5f7a76b1c037110b4c63073
James Walton <olottapi@gmail.com>
2014-10-09 23:03:14 -0400

    fixed the pins params...

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,38 +1,41 @@
 class PinsController < ApplicationController
-  respond_to :html
+ 
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   def index
     @pins = Pin.all
-    respond_with(@pins)
   end
 
   def show
-    respond_with(@pin)
   end
 
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+    redirect_to @pin, notice: 'Pin was successfully created.'
+  else
+    render action: 'new'
   end
+end
 
   def update
-    @pin.update(pin_params)
-    respond_with(@pin)
+    if @pin.update(pin_params)
+    redirect_to @pin, notice: 'Pin was successfully updated.'
+  else 
+    render action: 'edit'
+  end
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    redirect_to @pin
   end
 
   private
@@ -40,7 +43,12 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
