0edcaedf1009ca0e578519d4f4cff3c24328b315
James Walton <olottapi@gmail.com>
2014-10-17 14:17:55 -0400

    cleaned up more pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
          has_many :pins
          validates :name, presence: true
+         has_attached_file :image
+         validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+		 validates :image, presence: true
 end
