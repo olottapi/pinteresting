e618f75c4b14176e5bcdc89f8059ee91426db995
James Walton <olottapi@gmail.com>
2014-10-21 23:04:25 -0400

    cleaned up the images on profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,7 @@ class User < ActiveRecord::Base
 
          has_many :pins
          validates :name, presence: true
-         has_attached_file :image
-         validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-
          has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+         validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 		 
 end
